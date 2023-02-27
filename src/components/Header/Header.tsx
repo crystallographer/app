988098f8357e97f4c156b3257649b4f11d0e9d25
crystallographer <r0475582@proton.me>
2023-02-27 22:06:33 +0400

    fix: extended header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,37 @@
-import { Container, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 import logo from "../../../assets/images/logo.png";
 
 export function Header() {
     return (
-        <Navbar bg="primary" variant="dark" fixed="top">
+        <Navbar
+            bg="primary"
+            collapseOnSelect
+            expand="lg"
+            fixed="top"
+            variant="dark"
+        >
             <Container>
-                <Navbar.Brand href="#home">
+                <Navbar.Brand href="#">
                     <img
+                        alt="App logo"
+                        className="d-inline-block align-top"
+                        height="32"
                         src={logo}
                         width="32"
-                        height="32"
-                        className="d-inline-block align-top"
-                        alt="App logo"
                     />{" "}
                     App
                 </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#albums">Albums</Nav.Link>
+                        <Nav.Link href="#comments">Comments</Nav.Link>
+                        <Nav.Link href="#photos">Photos</Nav.Link>
+                        <Nav.Link href="#posts">Posts</Nav.Link>
+                        <Nav.Link href="#users">Users</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
